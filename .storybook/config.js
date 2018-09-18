8c23394d7af270d8f871c32fafbdbf6c6843e986
Nick McCurdy <nick@nickmccurdy.com>
2018-09-18 17:18:45 -0400

    Remove outdated comment

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,5 @@
 import { configure } from '@storybook/react';
 
-// automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /.story.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
