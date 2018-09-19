ed33c29b97f67a57df756b81699934da107b8ef7
Nick McCurdy <nick@nickmccurdy.com>
2018-09-19 16:45:47 -0400

    Automatically document props in info with react-docgen-typescript-loader

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,8 +1,13 @@
 module.exports = (baseConfig, env, config) => {
   config.module.rules.push({
     test: /\.tsx?$/,
-    loader: 'ts-loader',
-    options: { compilerOptions: { declaration: false } }
+    use: [
+      {
+        loader: 'ts-loader',
+        options: { compilerOptions: { declaration: false } }
+      },
+      'react-docgen-typescript-loader'
+    ]
   });
   config.resolve.extensions.push('.ts', '.tsx');
   return config;
