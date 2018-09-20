c70ebfa8c6876e3a280b11511ef17ba05eca2149
Nick McCurdy <nick@nickmccurdy.com>
2018-09-20 04:47:11 -0400

    Customize name and hide panel with options addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,14 @@
 import { setDefaults } from '@storybook/addon-info';
+import { setOptions } from '@storybook/addon-options';
 import { addDecorator, configure } from '@storybook/react';
 
 setDefaults({ header: false, inline: true });
+
+setOptions({
+  name: 'MyCrypto UI',
+  url: 'https://github.com/MyCryptoHQ/ui',
+  showAddonPanel: false
+});
 
 const req = require.context('../src', true, /.story.[jt]sx?$/);
 function loadStories() {
