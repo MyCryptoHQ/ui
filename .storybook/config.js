284df019abfd66526358ed6e9e06c70c73834fe8
Nick McCurdy <nick@nickmccurdy.com>
2018-09-20 04:28:51 -0400

    Show info inline

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,7 @@
-import { configure } from '@storybook/react';
+import { setDefaults } from '@storybook/addon-info';
+import { addDecorator, configure } from '@storybook/react';
+
+setDefaults({ header: false, inline: true });
 
 const req = require.context('../src', true, /.story.[jt]sx?$/);
 function loadStories() {
