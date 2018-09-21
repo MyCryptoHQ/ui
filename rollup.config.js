9e76a959682b2d0b1be69485933368f5da8bbbd8
Nick McCurdy <nick@nickmccurdy.com>
2018-09-21 01:02:10 -0400

    Fix import errors by using the boilerplate's Rollup config with PostCSS

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import postcss from 'rollup-plugin-postcss';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+
+/**
+ * Include all of the dependencies here to exclude all node modules from the build
+ * Make sure to also include node libraries you're using e.g. 'crypto'
+ */
+
+const external = [
+  ...Object.keys(pkg.dependencies),
+  ...Object.keys(pkg.peerDependencies),
+];
+
+/**
+ * Include all of the dependencies again here to squash rollup warnings
+ */
+const globals = {};
+
+export default {
+  input: `src/index.ts`,
+
+  output: { file: pkg.module, format: 'es', globals, sourcemap: true },
+
+  // exclude all node modules
+  external,
+
+  watch: {
+    include: 'src/**',
+  },
+  plugins: [
+    // Compile styles
+    postcss(),
+
+    // Compile TypeScript files
+    typescript(),
+
+    // Resolve source maps to the original source
+    sourceMaps(),
+  ],
+};
