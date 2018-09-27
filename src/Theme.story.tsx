273272b8a8daaaef71a51c7e3fc45eb4409c1908
Nick McCurdy <nick@nickmccurdy.com>
2018-09-27 16:51:54 -0400

    Clean up the Theme story styles with flexbox

--- src/Theme.story.tsx
+++ src/Theme.story.tsx
@@ -5,31 +5,30 @@ import styled from './styled-components';
 import Theme from './Theme';
 
 const Color = styled.div`
-  vertical-align: middle;
   background-color: ${props => props.color};
   border: 1px solid ${props => props.theme.textColor};
-  display: inline-block;
   height: 25px;
   width: 25px;
   margin: 5px 10px;
   border-radius: 50%;
 `;
 
-const Property = styled.code`
-  vertical-align: middle;
+const VerticallyCentered = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
-const ColorPair = withTheme(({ theme }: { theme: Theme }) => (
+const ThemePreview = withTheme(({ theme }: { theme: Theme }) => (
   <>
     {Object.entries(theme)
       .filter(([key]) => key.toLowerCase().includes('color'))
       .map(([key, value]) => (
-        <div key={key}>
+        <VerticallyCentered key={key}>
           <Color color={value} />
-          <Property>{key}</Property>
-        </div>
+          <code>{key}</code>
+        </VerticallyCentered>
       ))}
   </>
 ));
 
-storiesOf('Styles', module).add('Theme', () => <ColorPair />);
+storiesOf('Styles', module).add('Theme', () => <ThemePreview />);
