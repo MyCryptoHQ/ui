aa72299e8b1edfa4a68c0cf4f16e4d2826bf618b
Nick McCurdy <nick@nickmccurdy.com>
2018-09-28 19:00:09 -0400

    Improve button styles and add hover/focus/active variants

--- src/Theme.ts
+++ src/Theme.ts
@@ -4,13 +4,17 @@ export default interface Theme {
   background: string;
   name: string;
   primary: string;
+  primaryDark: string;
+  primaryDarker: string;
   text: string;
 }
 
 export const light: Theme = {
   background: '#ffffff',
   name: 'Light',
-  primary: '#027896',
+  primary: '#007896',
+  primaryDark: '#006077',
+  primaryDarker: '#004759',
   text: '#333',
 };
 
@@ -18,5 +22,7 @@ export const dark: Theme = {
   background: '#21252b',
   name: 'Dark',
   primary: '#0090b5',
+  primaryDark: '#007896',
+  primaryDarker: '#006077',
   text: '#ffffff',
 };
--- src/atoms/Button/Button.tsx
+++ src/atoms/Button/Button.tsx
@@ -1,16 +1,41 @@
-import styled from '../../styled-components';
+import styled, { css } from '../../styled-components';
 
-export const StyledButton = styled.button`
+const largeMixin = css`
+  font-size: 1.15rem;
+  line-height: 1.2;
+  padding: 1rem 2rem;
+`;
+
+export const Button = styled.button<{ large?: boolean }>`
   background-color: ${props => props.theme.primary};
+  border-color: ${props => props.theme.primary};
   border-radius: 2px;
-  border: 0;
+  border: 1px solid transparent;
   color: #ffffff;
   font-family: Lato, sans-serif;
   font-size: 18px;
-  height: 50px;
-  width: 200px;
+  line-height: 1.4;
+  padding: 0.75rem 2rem;
+  transition: opacity 0.12s ease, color 0.12s ease, background-color 0.12s ease,
+    border-color 0.12s ease;
+  user-select: none;
+
+  ${props => props.large && largeMixin};
+
+  &:focus,
+  &:hover {
+    background-color: ${props => props.theme.primaryDark};
+    border-color: ${props => props.theme.primaryDark};
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);
+  }
+
+  &:active {
+    background-color: ${props => props.theme.primaryDarker};
+    border-color: ${props => props.theme.primaryDarker};
+    box-shadow: inset 0 3px 5px rgba(0, 0, 0, 0.125);
+  }
 `;
 
-export default StyledButton;
+export default Button;
 
-StyledButton.defaultProps = { type: 'button' };
+Button.defaultProps = { type: 'button' };
