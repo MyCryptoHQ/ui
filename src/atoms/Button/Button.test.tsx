504bb5f7b6beb21e708430f43dba68dc7e0ec8fb
Nick McCurdy <nick@nickmccurdy.com>
2018-09-28 23:58:44 -0400

    Remove redundant expectation

--- src/atoms/Button/Button.test.tsx
+++ src/atoms/Button/Button.test.tsx
@@ -8,7 +8,6 @@ test('Button', () => {
   const { getByText } = render(<Button onClick={handleClick}>Button</Button>);
   const button = getByText('Button');
   expect(button).toHaveAttribute('type', 'button');
-  expect(button).toHaveTextContent('Button');
   fireEvent.click(button);
   expect(handleClick).toHaveBeenCalled();
 });
