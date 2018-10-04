05a0c3deee800e620c81ab4a5f22b7e46fb38f8f
Nick McCurdy <nick@nickmccurdy.com>
2018-10-04 16:32:09 -0400

    Add Input padding

--- src/atoms/Input/Input.tsx
+++ src/atoms/Input/Input.tsx
@@ -8,6 +8,7 @@ export const Input = styled(Text.withComponent('input'))`
   box-shadow: inset 0 1px 1px 0 rgba(63, 63, 68, 0.05);
   font-size: 1em;
   font-weight: bold;
+  padding: 0.75rem 1rem;
 `;
 
 export default Input;
