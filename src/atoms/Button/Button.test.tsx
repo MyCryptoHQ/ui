3d1a2b1d6db776800f98736e5a8c3f381410c5a5
Nick McCurdy <nick@nickmccurdy.com>
2018-10-04 18:30:00 -0400

    Smoke test Button's large prop

--- src/atoms/Button/Button.test.tsx
+++ src/atoms/Button/Button.test.tsx
@@ -5,9 +5,12 @@ import Button from '.';
 
 test('Button', () => {
   const handleClick = jest.fn();
-  const { getByText } = render(<Button onClick={handleClick}>Button</Button>);
+  const { getByText, rerender } = render(
+    <Button onClick={handleClick}>Button</Button>,
+  );
   const button = getByText('Button');
   expect(button).toHaveAttribute('type', 'button');
   fireEvent.click(button);
   expect(handleClick).toHaveBeenCalled();
+  rerender(<Button large={true} />);
 });
