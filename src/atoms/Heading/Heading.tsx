44c06c3b3cba0eea95bb7b9988777e35fe7bf880
Nick McCurdy <nick@nickmccurdy.com>
2018-10-05 17:44:10 -0400

    Remove redundant styles from Heading

--- src/atoms/Heading/Heading.tsx
+++ src/atoms/Heading/Heading.tsx
@@ -10,13 +10,10 @@ const scale = (level: number) => modularScale(level, undefined, 1.5);
 const Heading1 = styled(Text.withComponent('h1'))`
   color: ${props => props.theme.headline};
   font-size: ${scale(2)};
-  font-weight: bold;
 `;
 
 const Heading2 = styled(Text.withComponent('h2'))`
-  color: ${props => props.theme.text};
   font-size: ${scale(1)};
-  font-weight: bold;
 `;
 
 export function Heading({
