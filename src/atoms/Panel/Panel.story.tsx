da77ae03ac522c2ee1f80636cea1be25c090a88e
Nick McCurdy <nick@nickmccurdy.com>
2018-10-07 14:15:03 -0400

    Use Faker to generate random lorem ipsum paragraphs

--- src/atoms/Panel/Panel.story.tsx
+++ src/atoms/Panel/Panel.story.tsx
@@ -1,4 +1,5 @@
 import { storiesOf } from '@storybook/react';
+import faker from 'faker';
 import React from 'react';
 import Panel from '.';
 import styled from '../../styled-components';
@@ -10,14 +11,6 @@ const TextWithoutMargin = styled(Text)`
 
 storiesOf('Atoms', module).add('Panel', () => (
   <Panel>
-    <TextWithoutMargin>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </TextWithoutMargin>
+    <TextWithoutMargin>{faker.lorem.paragraphs()}</TextWithoutMargin>
   </Panel>
 ));
--- src/atoms/Text/Text.story.tsx
+++ src/atoms/Text/Text.story.tsx
@@ -1,14 +1,8 @@
 import { storiesOf } from '@storybook/react';
+import faker from 'faker';
 import React from 'react';
 import Text from '.';
 
 storiesOf('Atoms', module).add('Text', () => (
-  <Text>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  </Text>
+  <Text>{faker.lorem.paragraphs()}</Text>
 ));
