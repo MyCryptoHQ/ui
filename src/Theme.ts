382e2bd12dfdb4fcf07210d87996b7dffeddd5de
Nick McCurdy <nick@nickmccurdy.com>
2018-10-16 19:11:49 -0400

    Fix dark primary Theme colors by making them relative

--- src/Theme.ts
+++ src/Theme.ts
@@ -1,7 +1,8 @@
-import { invert, modularScale } from 'polished';
+import { darken, invert, lighten, modularScale } from 'polished';
 
+const lightnessMod = 0.06;
+const primary = '#007896';
 export const scale = (steps: number) => modularScale(steps, undefined, 1.5);
-
 export const transitionDuration = '0.12s';
 
 type Theme = Record<
@@ -27,9 +28,9 @@ export const light: Theme = {
   headline: '#163150',
   name: 'Light',
   panelBackground: 'white',
-  primary: '#007896',
-  primaryDark: '#006077',
-  primaryDarker: '#004759',
+  primary,
+  primaryDark: darken(lightnessMod, primary),
+  primaryDarker: darken(lightnessMod * 2, primary),
   text: '#424242',
 };
 
@@ -40,8 +41,8 @@ export const dark: Theme = {
   headline: invert(light.headline),
   name: 'Dark',
   panelBackground: '#282c34',
-  primary: '#007896',
-  primaryDark: '#007896',
-  primaryDarker: '#006077',
+  primary: lighten(lightnessMod, primary),
+  primaryDark: primary,
+  primaryDarker: darken(lightnessMod, primary),
   text: 'white',
 };
