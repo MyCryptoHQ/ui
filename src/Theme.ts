2472ec3804e192a109906fa044bbfcccbede77a0
Nick McCurdy <nick@nickmccurdy.com>
2018-10-16 19:23:06 -0400

    Move Theme name first for readability

--- src/Theme.ts
+++ src/Theme.ts
@@ -6,11 +6,11 @@ export const scale = (steps: number) => modularScale(steps, undefined, 1.5);
 export const transitionDuration = '0.12s';
 
 type Theme = Record<
+  | 'name'
   | 'background'
   | 'controlBackground'
   | 'controlBorder'
   | 'headline'
-  | 'name'
   | 'panelBackground'
   | 'primary'
   | 'primaryDark'
@@ -22,11 +22,12 @@ type Theme = Record<
 export default Theme;
 
 export const light: Theme = {
+  name: 'Light',
+
   background: 'white',
   controlBackground: 'white',
   controlBorder: '#e5ecf3',
   headline: '#163150',
-  name: 'Light',
   panelBackground: 'white',
   primary,
   primaryDark: darken(lightnessMod, primary),
@@ -35,11 +36,12 @@ export const light: Theme = {
 };
 
 export const dark: Theme = {
+  name: 'Dark',
+
   background: '#21252b',
   controlBackground: '#393f4c',
   controlBorder: '#4d5463',
   headline: invert(light.headline),
-  name: 'Dark',
   panelBackground: '#282c34',
   primary: lighten(lightnessMod, primary),
   primaryDark: primary,
