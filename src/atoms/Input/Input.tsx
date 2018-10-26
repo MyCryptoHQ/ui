faeaa56cd61db93e363d233719feca47d9bece89
Nick McCurdy <nick@nickmccurdy.com>
2018-10-25 21:40:00 -0400

    Replace Input shadows with a flat border

--- src/atoms/Input/Input.tsx
+++ src/atoms/Input/Input.tsx
@@ -1,21 +1,19 @@
-import { padding, transitions } from 'polished';
+import { padding } from 'polished';
 import styled from '../../styled-components';
 import { borderRadius, scale, transitionDuration } from '../../Theme';
 import Text from '../Text';
 
 export const Input = styled(Text.withComponent('input'))`
   background: ${props => props.theme.controlBackground};
-  border: 0.0625em solid ${props => props.theme.controlBorder};
+  border: 0.125em solid ${props => props.theme.controlBorder};
   border-radius: ${borderRadius};
-  box-shadow: inset 0 0.0625em 0.0625em 0 rgba(63, 63, 68, 0.05);
   font-size: ${scale(0)};
   font-weight: bold;
   ${padding(scale(-1), scale(0))};
-  ${transitions(['border', 'box-shadow'], transitionDuration)};
+  transition: border ${transitionDuration};
 
   :focus {
     border-color: ${props => props.theme.primary};
-    box-shadow: inset 0 0 0 0.0625em ${props => props.theme.primary};
     outline: none;
   }
 `;
