9da47994a0718e355d1be753af63c6146740a095
Jonathan Chhabra <Sonicrida@users.noreply.github.com>
2018-11-09 02:22:33 -0500

    Remove unused Link component since we are nesting the anchor tag styles in the Typography component

--- src/atoms/Link/Link.story.tsx
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import React from 'react';
-import Link from '.';
-
-storiesOf('Atoms', module).add('Link', () => (
-  <Link href="https://example.com">Link Text</Link>
-));
