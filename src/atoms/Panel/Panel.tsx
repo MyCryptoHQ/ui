070185c9d4b937099909736f8c6fedff9a99cddd
Nick McCurdy <nick@nickmccurdy.com>
2018-11-12 19:42:36 -0500

    Tweak conditional check for isPlaceholder

--- src/atoms/Panel/Panel.tsx
+++ src/atoms/Panel/Panel.tsx
@@ -14,9 +14,7 @@ export const Panel = styled.section<{
         ? props.theme.panelBackgroundDark
         : props.theme.panelBackground};
       border-radius: ${borderRadiusLarge};
-      box-shadow: ${props.isPlaceholder
-        ? 'none'
-        : '0 0.1875em 0.375em 0 #00000012'};
+      ${!props.isPlaceholder && 'box-shadow: 0 0.1875em 0.375em 0 #00000012'};
       margin-bottom: ${scale(0)};
       ${!props.noPadding && padding(scale(1), scale(2))};
     `};
