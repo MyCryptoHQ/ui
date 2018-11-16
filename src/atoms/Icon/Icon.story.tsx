fd6cafa12251017b7aed719bcb5b5d409bb5ca80
Jonathan Chhabra <Sonicrida@users.noreply.github.com>
2018-11-15 22:18:33 -0500

    Testing the storybook chapters addon

--- src/atoms/Icon/Icon.story.tsx
+++ src/atoms/Icon/Icon.story.tsx
@@ -1,17 +1,39 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-import Icon, { IconName, icons } from '.';
-import Typography from '../Typography';
+// import Icon, { IconName, icons } from '.';
+// import Typography from '../Typography';
 
-storiesOf('Atoms', module).add('Icon', () =>
-  icons.map(({ iconName }) => (
-    <Typography key={iconName} as="span">
-      <Icon
-        key={iconName}
-        icon={iconName as IconName}
-        title={iconName}
-        size="3x"
-      />
-    </Typography>
-  )),
-);
+// storiesOf('Atoms', module).add('Icon', () =>
+//   icons.map(({ iconName }) => (
+//     <Typography key={iconName} as="span">
+//       <Icon
+//         key={iconName}
+//         icon={iconName as IconName}
+//         title={iconName}
+//         size="3x"
+//       />
+//     </Typography>
+//   )),
+// );
+
+(storiesOf('Atoms', module) as any).addWithChapters('Icons', {
+  subtitle: 'Icon',
+  info: 'Renders an icon',
+  chapters: [
+    {
+      title: 'Default Icon',
+      sections: [
+        {
+          title: 'Default Icon',
+          sectionFn: () => <div>Some text</div>,
+          options: {
+            showSource: true,
+            allowSourceToggling: true,
+            showPropTables: true,
+            allowPropTablesToggling: true,
+          },
+        },
+      ],
+    },
+  ],
+});
