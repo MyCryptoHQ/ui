cb20a9f682def8962090b1360989cacc706ad657
Nick McCurdy <nick@nickmccurdy.com>
2018-11-16 21:00:55 -0500

    Delete comment with incorrect type

--- src/react-storybook-addon-chapters.d.ts
+++ src/react-storybook-addon-chapters.d.ts
@@ -1,10 +1,6 @@
 import '@storybook/react';
 
 declare module '@storybook/react' {
-  //   export function addWithChapters(
-  //     chapterName: string,
-  //     chapters: object,
-  //   ): object;
   export interface Story {
     addWithChapters(chapterName: string, chapters: object): object;
   }
