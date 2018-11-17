4f837c1a0c98b51c9aa68c2da605861d2fb2898f
Jonathan Chhabra <Sonicrida@users.noreply.github.com>
2018-11-17 05:19:08 -0500

    Create example of storybook chapters with the Icon component

--- src/atoms/Icon/Icon.story.tsx
+++ src/atoms/Icon/Icon.story.tsx
@@ -1,39 +1,31 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-// import Icon, { IconName, icons } from '.';
-// import Typography from '../Typography';
-
-// storiesOf('Atoms', module).add('Icon', () =>
-//   icons.map(({ iconName }) => (
-//     <Typography key={iconName} as="span">
-//       <Icon
-//         key={iconName}
-//         icon={iconName as IconName}
-//         title={iconName}
-//         size="3x"
-//       />
-//     </Typography>
-//   )),
-// );
+import Icon, { IconName, icons } from '.';
+import Typography from '../Typography';
 
 storiesOf('Atoms', module).addWithChapters('Icons', {
-  subtitle: 'Icon',
   info: 'Renders an icon',
   chapters: [
     {
-      title: 'Default Icon',
-      sections: [
-        {
-          title: 'Default Icon',
-          sectionFn: () => <div>Some text</div>,
-          options: {
-            showSource: true,
-            allowSourceToggling: true,
-            showPropTables: true,
-            allowPropTablesToggling: true,
-          },
+      sections: icons.map(({ iconName }) => ({
+        title: iconName,
+        sectionFn: () => (
+          <Typography key={iconName} as="span">
+            <Icon
+              key={iconName}
+              icon={iconName as IconName}
+              title={iconName}
+              size="2x"
+            />
+          </Typography>
+        ),
+        options: {
+          showSource: false,
+          allowSourceToggling: true,
+          showPropTables: false,
+          allowPropTablesToggling: false,
         },
-      ],
+      })),
     },
   ],
 });
