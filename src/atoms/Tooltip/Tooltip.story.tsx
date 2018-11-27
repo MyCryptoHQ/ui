250d81a345ce2efd583e230717018be767dc5abd
Nick McCurdy <nick@nickmccurdy.com>
2018-11-27 18:15:11 -0500

    Remove extra story offset

--- src/atoms/Tooltip/Tooltip.story.tsx
+++ src/atoms/Tooltip/Tooltip.story.tsx
@@ -7,11 +7,9 @@ import Tooltip from './Tooltip';
 storiesOf('Atoms', module).add('Tooltip', () => (
   <>
     <Typography>Before</Typography>
-    <Typography>Before</Typography>
     <Tooltip tooltip={<Typography as="div">Hello world!</Typography>}>
       {props => <Typography {...props}>Hover here!</Typography>}
     </Tooltip>
-    <Typography>After</Typography>
     <Typography>After</Typography>
   </>
 ));
