21cb4bf5b42315a1d02cf40e991ad93a2c6971cd
Jonathan Chhabra <Sonicrida@users.noreply.github.com>
2018-11-30 11:18:04 -0500

    Add Switch component for toggling settings

---
+++ src/atoms/Switch/Switch.test.tsx
@@ -0,0 +1,18 @@
+import 'jest-dom/extend-expect';
+import React from 'react';
+// import { fireEvent, render } from 'react-testing-library';
+import { render } from 'react-testing-library';
+
+import { Switch } from 'atoms';
+
+test('Switch', () => {
+  const handleClick = jest.fn();
+  //   const { getByTestId } = render(
+  //     <Switch data-testid="toggle" handleChange={handleClick} />,
+  //   );
+  //   const Toggle = getByTestId('toggle');/
+
+  //   fireEvent.click(Toggle);
+  //   expect(handleClick).toHaveBeenCalled();
+  render(<Switch data-testid="toggle" handleChange={handleClick} />);
+});
