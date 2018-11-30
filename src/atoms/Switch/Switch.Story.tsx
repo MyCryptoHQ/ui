67c48b73becde509220eb24c4cf56e2edf7c8fb4
Jonathan Chhabra <Sonicrida@users.noreply.github.com>
2018-11-30 11:35:24 -0500

    Add Switch component for toggling settings

---
+++ src/atoms/Switch/Switch.Story.tsx
@@ -0,0 +1,17 @@
+import { storiesOf } from '@storybook/react';
+import React, { Fragment } from 'react';
+
+import { Switch } from 'atoms';
+storiesOf('Atoms', module).add('Switch', () =>
+  [
+    {
+      handleChange: () => console.log('change detected'),
+      labelLeft: 'On',
+      labelRight: 'Off',
+    },
+  ].map((props, index) => (
+    <Fragment key={index}>
+      <Switch {...props} />
+    </Fragment>
+  )),
+);
