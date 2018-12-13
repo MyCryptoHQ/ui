1550b855afd44f7790cf9e45aa3ef49ad6165860
Nick McCurdy <nick@nickmccurdy.com>
2018-12-13 00:43:08 -0500

    Add padding and a background with better contrast to Icon story

--- src/atoms/Icon/Icon.story.tsx
+++ src/atoms/Icon/Icon.story.tsx
@@ -1,10 +1,22 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 
+import styled from '_styled-components';
+import { scale } from 'Theme';
 import Icon, { icons } from './Icon';
 
-storiesOf('Atoms', module).add('Icon', () =>
-  Object.keys(icons).map(icon => (
-    <Icon key={icon} icon={icon as keyof typeof icons} />
-  )),
-);
+const Container = styled.div`
+  background: #027796;
+`;
+
+const PaddedIcon = styled(Icon)`
+  padding: ${scale(1)};
+`;
+
+storiesOf('Atoms', module).add('Icon', () => (
+  <Container>
+    {Object.keys(icons).map(icon => (
+      <PaddedIcon key={icon} icon={icon as keyof typeof icons} />
+    ))}
+  </Container>
+));
