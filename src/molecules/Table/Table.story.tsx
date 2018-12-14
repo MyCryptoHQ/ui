0b80cc5700f76eec751dfdeb9874cc16cc66acc8
Connor Bryan <cchromium@gmail.com>
2018-12-14 12:43:05 -0600

    Implement Table

--- src/molecules/Table/Table.story.tsx
+++ src/molecules/Table/Table.story.tsx
@@ -1,19 +1,125 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 
+import { Button, Icon, Identicon } from 'atoms';
+import { Copyable } from 'molecules';
 import Table from './Table';
 
-const data = {
-  head: ['Name', 'Age'],
-  body: [['Connor', '26'], ['Dave', '30'], ['Steve', '32']],
+interface TableGroup {
+  title: string;
+  entries: any[][];
+  offset?: number;
+}
+
+interface TableData {
+  head: any[];
+  body: any[][];
+  groups?: TableGroup[];
+}
+
+const address = '0x80200997f095da94E404F7E0d581AAb1fFba9f7d';
+const truncate = (text: string): string => text.substr(0, 6);
+const accountTable: TableData = {
+  head: ['', 'Label', 'Address', 'Network', 'Value'],
+  body: [
+    [
+      <Icon key={0} icon="star" />,
+      <div
+        key={1}
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <Identicon
+          address={address}
+          style={{
+            width: '35px',
+            height: '35px',
+            marginRight: '1rem',
+          }}
+        />
+        Squidward's Emergency Fund
+      </div>,
+      <Copyable key={2} text={address} truncate={truncate} />,
+      'Ethereum',
+      '$2,203.12',
+    ],
+  ],
+};
+const recentTransactionsTable: TableData = {
+  head: ['', 'Date', 'From Address', 'To Address', 'Amount'],
+  body: [],
   groups: [
     {
-      title: 'Prospects',
-      entries: [['Aaron', '18'], ['Craig', '20']],
+      title: 'Pending',
+      entries: [
+        [
+          <img key={0} src="https://placehold.it/50x50" alt="Example" />,
+          'Today',
+          <Copyable key={2} text={address} truncate={truncate} />,
+          <Copyable key={3} text={address} truncate={truncate} />,
+          '42.69 OMG',
+        ],
+      ],
+      offset: 1,
+    },
+    {
+      title: 'Completed',
+      entries: [
+        [
+          <img key={0} src="https://placehold.it/50x50" alt="Example" />,
+          'Yesterday',
+          <Copyable key={2} text={address} truncate={truncate} />,
+          <Copyable key={3} text={address} truncate={truncate} />,
+          '13.37 OMG',
+        ],
+      ],
+      offset: 1,
     },
   ],
 };
+const addressBookTable: TableData = {
+  head: ['', 'Label', 'Address', 'Notes', ''],
+  body: [
+    [
+      <Icon key={0} icon="star" />,
+      <div
+        key={1}
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <Identicon
+          address={address}
+          style={{
+            width: '35px',
+            height: '35px',
+            marginRight: '1rem',
+          }}
+        />
+        Squidward's Emergency Fund
+      </div>,
+      <Copyable key={2} text={address} truncate={truncate} />,
+      'Spongebob needs to leave Bikini Bottom',
+      <Button key={4} basic={true}>
+        <Icon icon="times-circle" />
+      </Button>,
+    ],
+  ],
+};
 
-storiesOf('Molecules', module).add('Table', () => (
-  <Table head={data.head} body={data.body} groups={data.groups} />
-));
+storiesOf('Molecules', module).add('Table', () =>
+  [accountTable, recentTransactionsTable, addressBookTable].map(
+    (tableData, index) => (
+      <div key={index} style={{ marginBottom: '3rem' }}>
+        <Table
+          head={tableData.head}
+          body={tableData.body}
+          groups={tableData.groups}
+        />
+      </div>
+    ),
+  ),
+);
--- src/molecules/Table/Table.tsx
+++ src/molecules/Table/Table.tsx
@@ -1,9 +1,13 @@
 import styled from '_styled-components';
 import React, { Component } from 'react';
 
+import { Button, Icon } from 'atoms';
+import Typography from 'Typography';
+
 interface TableGroup {
   title: string;
   entries: any[][];
+  offset?: number;
 }
 
 interface Props {
@@ -12,31 +16,50 @@ interface Props {
   groups?: TableGroup[];
 }
 
+interface State {
+  collapsedGroups: {
+    [title: string]: true;
+  };
+}
+
 const TableHead = styled.tr`
   border-top: 1px solid #e8eaed;
   border-bottom: 1px solid #e8eaed;
   background: #fafcfc;
+` as any;
+
+const TableHeading = styled(Typography)`
+  min-width: 1rem;
+  padding: 15px 1rem 15px 0;
+  text-align: left;
   text-transform: uppercase;
 ` as any;
 
-const TableHeading = styled.th`
-  min-width: 3rem;
-  padding: 20px 0;
-  text-align: left;
-` as any;
+TableHeading.defaultProps = {
+  as: 'th',
+};
 
 const TableRow = styled.tr`
   border-bottom: 1px solid #e8eaed;
 ` as any;
 
-const TableCell = styled.td`
-  min-width: 5rem;
-  padding: 15px 0;
+const TableCell = styled(Typography)`
+  min-width: 1rem;
+  padding: 15px 1rem 15px 0;
 ` as any;
 
+TableCell.defaultProps = {
+  as: 'td',
+};
+
 class AbstractTable extends Component<Props> {
+  public state: State = {
+    collapsedGroups: {},
+  };
+
   public render() {
     const { head, body = [], groups = [], ...rest } = this.props;
+    const { collapsedGroups } = this.state;
 
     return (
       <table {...rest}>
@@ -47,30 +70,59 @@ class AbstractTable extends Component<Props> {
         </TableHead>
         <tbody>
           {body.map((row, rowIndex) => (
-            <TableRow key={rowIndex}>
-              {row.map((cell, cellIndex) => (
-                <TableCell key={cellIndex}>{cell}</TableCell>
-              ))}
-            </TableRow>
+            <React.Fragment key={rowIndex}>
+              <TableRow>
+                {row.map((cell, cellIndex) => (
+                  <TableCell key={cellIndex}>{cell}</TableCell>
+                ))}
+              </TableRow>
+            </React.Fragment>
           ))}
-          {groups.map(group => (
-            <React.Fragment key={group.title}>
-              <TableHead>
-                <TableHeading colSpan={head.length}>{group.title}</TableHeading>
-              </TableHead>
-              {group.entries.map((row, rowIndex) => (
-                <TableRow key={rowIndex}>
-                  {row.map((cell, cellIndex) => (
-                    <TableCell key={cellIndex}>{cell}</TableCell>
-                  ))}
-                </TableRow>
-              ))}
+          {groups.map(({ title, entries, offset = 0 }) => (
+            <React.Fragment key={title}>
+              <Button
+                as={TableRow}
+                basic={true}
+                onClick={this.toggleCollapseGroup.bind(this, title)}
+                style={{
+                  textTransform: 'uppercase',
+                }}
+              >
+                {Array.from({ length: offset }, (_, index) => (
+                  <td key={index} />
+                ))}
+                <TableHeading colSpan={head.length - offset}>
+                  {title}
+                  <Icon
+                    icon={collapsedGroups[title] ? 'caret-up' : 'caret-down'}
+                    style={{
+                      marginLeft: '0.5rem',
+                    }}
+                  />
+                </TableHeading>
+              </Button>
+              {!collapsedGroups[title] &&
+                entries.map((row, rowIndex) => (
+                  <TableRow key={rowIndex}>
+                    {row.map((cell, cellIndex) => (
+                      <TableCell key={cellIndex}>{cell}</TableCell>
+                    ))}
+                  </TableRow>
+                ))}
             </React.Fragment>
           ))}
         </tbody>
       </table>
     );
   }
+
+  private toggleCollapseGroup = (title: string) =>
+    this.setState((prevState: State) => ({
+      collapsedGroups: {
+        ...prevState.collapsedGroups,
+        [title]: !prevState.collapsedGroups[title],
+      },
+    }));
 }
 
 const Table = styled(AbstractTable)`
