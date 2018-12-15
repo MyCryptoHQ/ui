9086ec8050fa18150d89e928a57ff84726f908b7
Nick McCurdy <nick@nickmccurdy.com>
2018-12-14 19:32:00 -0500

    Add top-level export for Table

--- src/molecules/index.ts
+++ src/molecules/index.ts
@@ -2,3 +2,4 @@ export { default as ActionPanel } from './ActionPanel';
 export { default as Copyable } from './Copyable';
 export { default as IconLink } from './IconLink';
 export { default as List } from './List';
+export { default as Table } from './Table';
