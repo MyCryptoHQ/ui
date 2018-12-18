dab44b6fc73f704ac27780f2c826f4a834dc7459
Connor Bryan <cchromium@gmail.com>
2018-12-17 18:05:17 -0600

    Added support for groups

--- src/organisms/CollapsibleTable/CollapsibleTable.story.tsx
+++ src/organisms/CollapsibleTable/CollapsibleTable.story.tsx
@@ -3,14 +3,31 @@ import React from 'react';
 
 import CollapsibleTable, { CollapsibleTableData } from './CollapsibleTable';
 
-const data: CollapsibleTableData = {
+const basicData: CollapsibleTableData = {
   head: ['Name', 'Age'],
   body: [['Connor', '26'], ['David', '29']],
   config: {
     primaryColumn: 'Name',
   },
 };
+const groupData: CollapsibleTableData = {
+  head: ['Name', 'Age'],
+  body: [['Frank', '26']],
+  groups: [
+    {
+      title: 'Employees',
+      entries: [['Natalie', '22'], ['Andrea', '50']],
+    },
+  ],
+  config: {
+    primaryColumn: 'Name',
+  },
+};
 
-storiesOf('Organisms', module).add('CollapsibleTable', () => (
-  <CollapsibleTable {...data} />
-));
+storiesOf('Organisms', module).add('CollapsibleTable', () =>
+  [basicData, groupData].map((data, index) => (
+    <div key={index} style={{ marginBottom: '10rem' }}>
+      <CollapsibleTable {...data} />
+    </div>
+  )),
+);
--- src/organisms/CollapsibleTable/CollapsibleTable.tsx
+++ src/organisms/CollapsibleTable/CollapsibleTable.tsx
@@ -1,7 +1,11 @@
 import throttle from 'lodash/throttle';
 import React, { Component, ReactNode } from 'react';
 
+import styled from '_styled-components';
+import { Icon } from 'atoms';
 import { StackedCard, Table, TableConfig, TableData } from 'molecules';
+import { scale } from 'Theme';
+import Typography from 'Typography';
 
 export enum CollapsibleTableModes {
   Mobile,
@@ -21,39 +25,81 @@ export interface StackedCardData {
   entries: (string | ReactNode)[][];
 }
 
+interface CollapsedGroups {
+  [title: string]: boolean;
+}
+
 type Props = CollapsibleTableData;
 
 interface State {
   mode: CollapsibleTableModes;
+  collapsedGroups: CollapsedGroups;
 }
 
-export const transformTableToCards = ({
-  head,
-  body,
-  config,
-}: CollapsibleTableData): StackedCardData[] => {
+export const transformRowToCards = (
+  row: ReactNode[],
+  head: string[],
+  primaryColumnIndex: number,
+): StackedCardData =>
+  row.reduce(
+    (prev: StackedCardData, next, index) => {
+      if (index === primaryColumnIndex) {
+        prev.heading = next;
+      } else {
+        prev.entries.push([head[index], next]);
+      }
+
+      return prev;
+    },
+    {
+      heading: '',
+      entries: [],
+    } as StackedCardData,
+  );
+
+export const transformTableToCards = (
+  { head, body, groups = [], config }: CollapsibleTableData,
+  collapsedGroups: CollapsedGroups,
+): (StackedCardData | string)[] => {
   const { primaryColumn } = config;
   const primaryColumnIndex = head.indexOf(primaryColumn);
-  const cards = body.map((row, rowIndex) => {
-    return row.reduce(
-      (prev: StackedCardData, next, index) => {
-        if (index === primaryColumnIndex) {
-          prev.heading = next;
-        } else {
-          prev.entries.push([head[index], next]);
-        }
+  const cards: (StackedCardData | string)[] = body.map(row =>
+    transformRowToCards(row, head, primaryColumnIndex),
+  );
 
-        return prev;
-      },
-      {
-        heading: '',
-        entries: [],
-      } as StackedCardData,
-    );
-  });
+  groups.forEach(({ title, entries }) =>
+    cards.push(
+      title,
+      ...(collapsedGroups[title]
+        ? []
+        : entries.map(row =>
+            transformRowToCards(row, head, primaryColumnIndex),
+          )),
+    ),
+  );
 
   return cards;
 };
+
+const GroupHeading = styled(Typography)`
+  display: flex;
+  align-items: center;
+  margin: 0;
+  padding: 0.9375rem;
+  border-bottom: 0.0625rem solid #dde3ee;
+  background: ${props => props.theme.tableHeadBackground};
+  text-transform: uppercase;
+  font-size: ${scale(1)};
+  cursor: pointer;
+`;
+
+GroupHeading.defaultProps = {
+  role: 'button',
+};
+
+const GroupHeadingCaret = styled(Icon)`
+  margin-left: 0.5em;
+`;
 
 export class AbstractCollapsibleTable extends Component<Props, State> {
   public State = {
@@ -69,6 +115,7 @@ export class AbstractCollapsibleTable extends Component<Props, State> {
       mode: isMobile
         ? CollapsibleTableModes.Mobile
         : CollapsibleTableModes.Desktop,
+      collapsedGroups: {},
     };
 
     this.checkWindowSize = throttle(this.checkWindowSize, 200);
@@ -83,12 +130,24 @@ export class AbstractCollapsibleTable extends Component<Props, State> {
   }
 
   public render() {
-    const { mode } = this.state;
+    const { mode, collapsedGroups } = this.state;
 
     return mode === CollapsibleTableModes.Mobile ? (
-      transformTableToCards(this.props).map((cardData, index) => (
-        <StackedCard key={index} {...cardData} />
-      ))
+      transformTableToCards(this.props, collapsedGroups).map(
+        (cardData, index) =>
+          typeof cardData === 'string' ? (
+            <GroupHeading
+              onClick={this.toggleCollapseGroup.bind(this, cardData)}
+            >
+              {cardData}
+              <GroupHeadingCaret
+                icon={collapsedGroups[cardData] ? 'caret-up' : 'caret-down'}
+              />
+            </GroupHeading>
+          ) : (
+            <StackedCard key={index} {...cardData} />
+          ),
+      )
     ) : (
       <Table {...this.props} />
     );
@@ -100,11 +159,27 @@ export class AbstractCollapsibleTable extends Component<Props, State> {
     const isMobile = window.matchMedia('(max-width: 450px)').matches;
 
     if (wasMobile && !isMobile) {
-      this.setState({ mode: CollapsibleTableModes.Desktop });
+      // Mobile-to-Desktop
+      this.setState({
+        mode: CollapsibleTableModes.Desktop,
+        collapsedGroups: {},
+      });
     } else if (!wasMobile && isMobile) {
-      this.setState({ mode: CollapsibleTableModes.Mobile });
+      // Desktop-to-Mobile
+      this.setState({
+        mode: CollapsibleTableModes.Mobile,
+        collapsedGroups: {},
+      });
     }
   };
+
+  private toggleCollapseGroup = (title: string) =>
+    this.setState((prevState: State) => ({
+      collapsedGroups: {
+        ...prevState.collapsedGroups,
+        [title]: !prevState.collapsedGroups[title],
+      },
+    }));
 }
 
 const CollapsibleTable = AbstractCollapsibleTable;
