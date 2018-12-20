ea8e4c6b78bdff22c5610ee748eedfaf65aa24e5
Jonathan Chhabra <Sonicrida@users.noreply.github.com>
2018-12-20 05:21:10 -0500

    Add storybook viewport addon to test responsive styles

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,6 +5,7 @@ import { addDecorator, configure } from '@storybook/react';
 import { cover } from 'polished';
 import React, { StrictMode } from 'react';
 import { withThemesProvider } from 'storybook-addon-styled-component-theme';
+import { configureViewport } from '@storybook/addon-viewport';
 import 'typeface-lato';
 
 import styled from 'src/styled-components';
@@ -31,6 +32,35 @@ addDecorator(story => <Container>{story()}</Container>);
 addDecorator(withThemesProvider([light, dark]));
 
 addDecorator(story => <StrictMode>{story()}</StrictMode>);
+
+const defaultViewport = {
+  /**
+   * name to display in the dropdown
+   * @type {String}
+   */
+  name: 'Responsive',
+
+  /**
+   * Inline styles to be applied to the story (iframe).
+   * styles is an object whose key is the camelCased version of the style name, and whose
+   * value is the style’s value, usually a string
+   * @type {Object}
+   */
+  styles: {
+    width: '100%',
+    height: '100%',
+  },
+
+  /**
+   * type of the device (e.g. desktop, mobile, or tablet)
+   * @type {String}
+   */
+  type: 'desktop',
+};
+
+configureViewport({
+  viewports: defaultViewport,
+});
 
 const req = require.context('../src', true, /.story.[jt]sx?$/);
 function loadStories() {
