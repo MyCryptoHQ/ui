85298e1cf140f045d31b8c5bb352d0ec0b0101fe
Nick McCurdy <nick@nickmccurdy.com>
2019-01-09 21:43:28 -0500

    Move Rollup config into Gulp config and add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,49 @@
 /* eslint-disable import/no-commonjs */
 const gulp = require('gulp');
 const alias = require('mycrypto-path-alias-resolver/gulp');
+const rollup = require('rollup');
+const sourceMaps = require('rollup-plugin-sourcemaps');
+const typescript = require('rollup-plugin-typescript2');
+const url = require('rollup-plugin-url');
 
-gulp.task('default', () => {
+const pkg = require('./package.json');
+
+/**
+ * Include all of the dependencies here to exclude all node modules from the build
+ * Make sure to also include node libraries you're using e.g. 'crypto'
+ */
+
+const external = [
+  ...Object.keys(pkg.dependencies),
+  ...Object.keys(pkg.peerDependencies),
+];
+
+const input = {
+  input: 'src/index.ts',
+
+  // exclude all node modules
+  external,
+
+  plugins: [
+    // Bundle images
+    url(),
+
+    // Compile TypeScript files
+    typescript(),
+
+    // Resolve source maps to the original source
+    sourceMaps(),
+  ],
+};
+
+const output = { file: pkg.module, format: 'es', sourcemap: true };
+
+gulp.task('build', async function() {
+  const bundle = await rollup.rollup(input);
+  await bundle.write(output);
+});
+
+gulp.task('default', ['build'], () => {
   return gulp
     .src([
       './src/**/*.ts',
@@ -12,4 +53,15 @@ gulp.task('default', () => {
     ])
     .pipe(alias('.', { src: './src' }))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', () => {
+  rollup.watch({
+    ...input,
+    output,
+    watch: {
+      include: 'src/**',
+    },
+  });
+  gulp.watch('dist', ['default']);
 });
