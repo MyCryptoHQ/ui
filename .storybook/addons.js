933c7bae3618dffc0d772df0ee32e0386882ffb2
Nick McCurdy <nick@nickmccurdy.com>
2019-01-12 23:15:46 -0500

    Update Storybook for performance improvements (300% faster startup)

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,3 +1,3 @@
 import '@storybook/addon-a11y/register';
 import '@storybook/addon-options/register';
-import 'storybook-addon-styled-component-theme/dist/register';
+import 'storybook-addon-styled-component-theme/dist/src/register';
