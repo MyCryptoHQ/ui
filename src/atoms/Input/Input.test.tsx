69b0e8d0cffae2b5908977dd681149a2b9b153cd
Nick McCurdy <nick@nickmccurdy.com>
2019-01-15 16:20:35 -0500

    Add smoke test for validation

--- src/atoms/Input/Input.test.tsx
+++ src/atoms/Input/Input.test.tsx
@@ -3,6 +3,15 @@ import { render } from 'react-testing-library';
 
 import Input from './Input';
 
+function validator(value: string) {
+  if (!['Ethereum', 'Ropsten'].includes(value)) {
+    return 'Invalid selection, must choose Ethereum or Ropsten';
+  }
+}
+
 test('Input', () => {
   render(<Input />);
+  const { rerender } = render(<Input value="" validator={validator} />);
+  rerender(<Input value="Invalid" validator={validator} />);
+  rerender(<Input value="Ethereum" validator={validator} />);
 });
