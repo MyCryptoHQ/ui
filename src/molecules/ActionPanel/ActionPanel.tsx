c647f9a3f662a407f88f645fad4aff519e526103
Connor Bryan <cchromium@gmail.com>
2019-01-21 13:26:40 -0600

    Remove inherent linkification of action panel slot

--- src/molecules/ActionPanel/ActionPanel.tsx
+++ src/molecules/ActionPanel/ActionPanel.tsx
@@ -1,9 +1,5 @@
 import { padding } from 'polished';
-import React, {
-  AnchorHTMLAttributes,
-  DetailedHTMLProps,
-  ReactNode,
-} from 'react';
+import React, { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
 import { StyledComponentClass } from 'styled-components';
 
 import { Panel } from 'src/atoms';
@@ -16,45 +12,33 @@ const ActionPanelBody = styled.div`
   ${padding(scale(1), scale(2))};
 `;
 
-const ActionPanelLink = styled(Typography)`
+const ActionPanelFooter = styled(Typography)`
   background: ${props => props.theme.actionPanelBackground};
   border-top: 0.0416em solid ${props => props.theme.actionPanelBorder};
-  color: ${props => props.theme.link};
   display: block;
-  font-size: ${scale(1)};
-  font-weight: bold;
   padding: ${scale(1)};
-  text-align: center;
   text-decoration: none;
-
-  :hover {
-    color: ${props => props.theme.linkHover};
-  }
 ` as StyledComponentClass<
-  DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>,
+  DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>,
   Theme
 >;
 
-ActionPanelLink.defaultProps = { as: 'a' };
+ActionPanelFooter.defaultProps = { as: 'div' };
 
 export function ActionPanel({
   action,
   children,
-  href,
   noPadding,
   ...rest
 }: {
   action: string;
   children: ReactNode;
-  href: string;
   noPadding?: boolean;
 } & ExtractProps<typeof Panel>) {
   return (
     <Panel noPadding={true} {...rest}>
       {noPadding ? children : <ActionPanelBody>{children}</ActionPanelBody>}
-      <footer>
-        <ActionPanelLink href={href}>{action}</ActionPanelLink>
-      </footer>
+      <ActionPanelFooter>{action}</ActionPanelFooter>
     </Panel>
   );
 }
