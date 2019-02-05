dc80da1d621f873fef533f5afc0fad17c1f4897f
Jonathan Chhabra <Sonicrida@users.noreply.github.com>
2019-02-05 14:58:45 -0500

    Fix heading bug

--- src/Typography/Typography.tsx
+++ src/Typography/Typography.tsx
@@ -2,7 +2,7 @@ import { fluidRange, transparentize } from 'polished';
 
 import styled from 'src/styled-components';
 
-export const Typography = styled.p<{ muted?: boolean }>`
+export const Typography = styled.p<{ muted?: boolean; as?: string }>`
   color: ${props =>
     props.muted ? transparentize(0.25, props.theme.text) : props.theme.text};
   ${fluidRange(
@@ -12,7 +12,20 @@ export const Typography = styled.p<{ muted?: boolean }>`
   )};
   font-family: Lato, sans-serif;
   line-height: 1.5;
-
+  ${props =>
+    props.as === 'h2' &&
+    fluidRange(
+      { prop: 'font-size', fromSize: '24px', toSize: '36px' },
+      '400px',
+      '1000px',
+    )};
+  ${props =>
+    props.as === 'h1' &&
+    fluidRange(
+      { prop: 'font-size', fromSize: '36px', toSize: '48px' },
+      '400px',
+      '1000px',
+    )};
   a {
     color: ${props => props.theme.link};
     text-decoration: none;
