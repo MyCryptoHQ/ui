935799bf81d14733b5f7df0c8e695dda2e9ef45c
Azarielle <sharon.manrique2@hotmail.com>
2019-03-06 19:34:58 -0500

    button test refactor

--- src/atoms/Button/Button.test.tsx
+++ src/atoms/Button/Button.test.tsx
@@ -1,20 +1,39 @@
 import 'jest-dom/extend-expect';
 import React from 'react';
-import { fireEvent, render } from 'react-testing-library';
+import { render } from 'react-testing-library';
 
 import Button from './Button';
 
-test('Button', () => {
+const context = describe;
+
+describe('Button tests', () => {
   const handleClick = jest.fn();
   const { getByText, rerender } = render(
     <Button onClick={handleClick}>Button</Button>,
   );
   const button = getByText('Button');
-  expect(button).toHaveAttribute('type', 'button');
-  fireEvent.click(button);
-  expect(handleClick).toHaveBeenCalled();
-  rerender(<Button large={true} />);
-  rerender(<Button secondary={true} />);
-  rerender(<Button basic={true} />);
-  rerender(<Button icon="copy" />);
+
+  context('smoke tests', () => {
+    it('should render properly', () => {
+      expect(button).toHaveAttribute('type', 'button');
+    });
+  });
+
+  context('size tests', () => {
+    it('should render as a large button', () => {
+      rerender(<Button large={true} />);
+    });
+
+    it('should render as a secondary button', () => {
+      rerender(<Button secondary={true} />);
+    });
+
+    it('should render as a basic button', () => {
+      rerender(<Button basic={true} />);
+    });
+
+    it('should show a "copy" icon', () => {
+      rerender(<Button icon="copy" />);
+    });
+  });
 });
