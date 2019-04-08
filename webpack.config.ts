33ea7c86a333db102ccfe81d12ee3272b2aaed1a
Luit Hollander <luit@hollander.email>
2019-04-08 22:17:06 +0200

    Remove nodejs build

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,20 +1,22 @@
-/* eslint-disable */
+/* eslint-disable import/no-nodejs-modules */
 import * as path from 'path';
 import { Configuration } from 'webpack';
-import * as merge from 'webpack-merge';
-import nodeExternals from 'webpack-node-externals';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 const sourcePath = path.resolve(__dirname, 'src');
 
 const config: Configuration = {
+  target: 'web',
   mode: isDevelopment ? 'development' : 'production',
   entry: {
     UI: path.resolve(sourcePath, 'index.ts'),
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
     library: 'UI',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
   },
   resolve: {
     extensions: ['.js', '.ts', '.tsx', '.svg'],
@@ -47,22 +49,4 @@ const config: Configuration = {
   devtool: 'source-map',
 };
 
-const browser: Configuration = merge.smart(config, {
-  target: 'web',
-  output: {
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
-    filename: '[name].js',
-  },
-});
-
-const node: Configuration = merge.smart(config, {
-  target: 'node',
-  externals: [nodeExternals()],
-  output: {
-    libraryTarget: 'commonjs2',
-    filename: '[name].node.js',
-  },
-});
-
-export default [browser, node];
+export default config;
