a5529422a17c31ddd899728890f392cc64edc121
Nick McCurdy <nick@nickmccurdy.com>
2019-06-10 13:08:32 -0400

    Test Dropdown's onChange handler

--- src/molecules/Dropdown/Dropdown.test.tsx
+++ src/molecules/Dropdown/Dropdown.test.tsx
@@ -1,9 +1,15 @@
 import 'jest-dom/extend-expect';
 import React from 'react';
-import { render } from 'react-testing-library';
+import { fireEvent, render } from 'react-testing-library';
 
 import Dropdown from './Dropdown';
 
 test('Dropdown', () => {
-  render(<Dropdown items={new Set(['Ethereum', 'Ropsten'])} />);
+  const items = new Set(['Ethereum', 'Ropsten']);
+  const handleChange = jest.fn();
+  const { getByText } = render(
+    <Dropdown items={items} onChange={handleChange} />,
+  );
+  fireEvent.click(getByText('Ethereum'));
+  expect(handleChange).toHaveBeenCalledWith(0);
 });
