3c8fd3f373b68dae51f8d973bf60fb555ed53e87
Frederik Bolding <frederik.bolding@gmail.com>
2021-04-07 16:26:16 +0200

    Tried to setup Storyshots and failed

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,10 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../src';
 
-import '@fontsource/lato/300.css';
+/**import '@fontsource/lato/300.css';
 import '@fontsource/lato/400.css';
 import '@fontsource/lato/700.css';
-import '@fontsource/roboto-mono/400.css';
+import '@fontsource/roboto-mono/400.css';**/
 
 export const decorators = [
   (Story) => (
