0c39c673f4e89d97704bc1260e5ea8f2823601bb
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-04-07 14:54:30 +0200

    Fix font loading in Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,11 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../src';
 
+import '@fontsource/lato/300.css';
+import '@fontsource/lato/400.css';
+import '@fontsource/lato/700.css';
+import '@fontsource/roboto-mono/400.css';
+
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
