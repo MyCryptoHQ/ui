14b3af76249782aa2e31d1387e6c6df0a5cf40a6
Frederik Bolding <frederik.bolding@gmail.com>
2021-05-03 16:06:42 +0200

    Add Spinner

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -2,6 +2,7 @@ export * from './Blockie';
 export * from './Typography';
 export * from './Address';
 export * from './Button';
+export { default as Spinner, SpinnerProps } from './Spinner';
 
 // Re-export as these should be used over the ones imported from rebass!
 export {
