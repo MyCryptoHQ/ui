a5ac7a6aff90cedd4c9acfd02cd20e5cc4b5f94c
Frederik Bolding <frederik.bolding@gmail.com>
2021-07-15 12:50:31 +0200

    Memoize blockie

--- src/atoms/Blockie.tsx
+++ src/atoms/Blockie.tsx
@@ -1,5 +1,6 @@
 import makeBlockie from 'ethereum-blockies-base64';
 import type { FunctionComponent } from 'react';
+import { useMemo } from 'react';
 
 import type { ImageProps } from '.';
 import { Image } from '.';
@@ -8,6 +9,7 @@ export interface BlockieProps extends ImageProps {
   address: string;
 }
 
-export const Blockie: FunctionComponent<BlockieProps> = ({ address, ...props }) => (
-  <Image src={makeBlockie(address)} variant="avatar" {...props} />
-);
+export const Blockie: FunctionComponent<BlockieProps> = ({ address, ...props }) => {
+  const blockie = useMemo(() => makeBlockie(address), [address]);
+  return <Image src={blockie} variant="avatar" {...props} />;
+};
