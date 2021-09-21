b0adf28c72ba3ce4fcf8a85d0425a9af371ecafa
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-09-21 16:11:20 +0200

    Add more icons

--- src/atoms/Icon.tsx
+++ src/atoms/Icon.tsx
@@ -2,14 +2,26 @@ import type { FunctionComponent } from 'react';
 
 import type { ImageProps } from '.';
 import { Image } from '.';
+import alert from '../assets/icons/alert.svg';
+import caret from '../assets/icons/caret.svg';
+import checkmark from '../assets/icons/checkmark.svg';
 import help from '../assets/icons/help.svg';
 import home from '../assets/icons/home.svg';
+import info from '../assets/icons/info.svg';
 import present from '../assets/icons/present.svg';
+import waiting from '../assets/icons/waiting.svg';
+import warning from '../assets/icons/warning.svg';
 
 export const icons = {
+  alert,
+  caret,
+  checkmark,
   help,
   home,
-  present
+  info,
+  present,
+  waiting,
+  warning
 };
 
 export type Icons = keyof typeof icons;
