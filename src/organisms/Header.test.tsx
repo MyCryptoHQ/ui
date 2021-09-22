2c561ced3e7443c31226fd455065eabeacce644a
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-09-22 13:07:48 +0200

    Pass single element instead of array

--- src/organisms/Header.test.tsx
+++ src/organisms/Header.test.tsx
@@ -1,8 +1,13 @@
 import { simpleRender } from '../../.jest/test-utils';
+import { Logo } from '../atoms';
 import { Header } from './Header';
 
 describe('Header', () => {
   it('renders correctly', () => {
-    expect(() => simpleRender(<Header />)).not.toThrow();
+    expect(() =>
+      simpleRender(
+        <Header leftComponents={<Logo />} centerComponents={<Logo />} rightComponents={<Logo />} />
+      )
+    ).not.toThrow();
   });
 });
--- src/organisms/Header.tsx
+++ src/organisms/Header.tsx
@@ -1,12 +1,12 @@
-import type { ComponentType, FunctionComponent } from 'react';
+import type { FunctionComponent, ReactElement } from 'react';
 import styled from 'styled-components';
 
 import { Box, Container, Flex } from '../atoms';
 
 export interface HeaderProps {
-  leftComponents?: ComponentType[];
-  centerComponents?: ComponentType[];
-  rightComponents?: ComponentType[];
+  leftComponents?: ReactElement;
+  centerComponents?: ReactElement;
+  rightComponents?: ReactElement;
 }
 
 export const HeaderWrapper = styled(Flex).attrs({ alignItems: 'center' })``;
@@ -29,25 +29,13 @@ export const Header: FunctionComponent<HeaderProps> = ({
     <Container display="flex" flexDirection="column" justifyContent="center" minHeight="90px">
       <Flex>
         <HeaderSection>
-          <HeaderWrapper mx="-16px">
-            {leftComponents?.map((component, index) => (
-              <Box key={`left-${index}`}>{component}</Box>
-            ))}
-          </HeaderWrapper>
+          <HeaderWrapper mx="-16px">{leftComponents ?? null}</HeaderWrapper>
         </HeaderSection>
         <HeaderSection>
-          <HeaderWrapper>
-            {centerComponents?.map((component, index) => (
-              <Box key={`center-${index}`}>{component}</Box>
-            ))}
-          </HeaderWrapper>
+          <HeaderWrapper>{centerComponents ?? null}</HeaderWrapper>
         </HeaderSection>
         <HeaderSection>
-          <HeaderWrapper mr="-16px">
-            {rightComponents?.map((component, index) => (
-              <Box key={`right-${index}`}>{component}</Box>
-            ))}
-          </HeaderWrapper>
+          <HeaderWrapper mr="-16px">{rightComponents ?? null}</HeaderWrapper>
         </HeaderSection>
       </Flex>
     </Container>
