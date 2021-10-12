f337ef0401e8615600d1675692eb9a72ce39e3ca
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-10-12 16:37:32 +0200

    Change icon on copy

--- src/atoms/Copyable.tsx
+++ src/atoms/Copyable.tsx
@@ -1,4 +1,5 @@
 import type { FunctionComponent } from 'react';
+import { useEffect, useState } from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
 
 import { Flex, Icon } from '.';
@@ -8,11 +9,32 @@ interface CopyableProps {
 }
 
 export const Copyable: FunctionComponent<CopyableProps> = ({ text, children }) => {
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    if (copied) {
+      const timeout = setTimeout(() => {
+        setCopied(false);
+      }, 2000);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [copied]);
+
+  const handleCopy = () => {
+    setCopied(true);
+  };
+
   return (
-    <CopyToClipboard text={text}>
+    <CopyToClipboard text={text} onCopy={handleCopy}>
       <Flex sx={{ cursor: 'pointer' }}>
         {children}
-        <Icon type="copy" ml="1" />
+        <Icon
+          type={copied ? 'checkmark' : 'copy'}
+          fill="#B5BFC7"
+          ml="1"
+          data-testid="copyable-icon"
+        />
       </Flex>
     </CopyToClipboard>
   );
