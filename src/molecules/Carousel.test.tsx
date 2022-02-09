6a1032aaa6f0401b942edb41badbed190b48fd1c
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-02-09 17:29:08 +0100

    Test clicking on carousel heading

--- src/molecules/Carousel.test.tsx
+++ src/molecules/Carousel.test.tsx
@@ -1,8 +1,8 @@
-import { simpleRender } from '../../.jest/test-utils';
+import { fireEvent, simpleRender } from '../../.jest/test-utils';
 import { Carousel } from './Carousel';
 
 describe('Carousel', () => {
-  jest.useFakeTimers();
+  afterEach(() => jest.useRealTimers());
 
   it('renders correctly', () => {
     expect(() =>
@@ -11,6 +11,8 @@ describe('Carousel', () => {
   });
 
   it('shows the current element', async () => {
+    jest.useFakeTimers();
+
     const { findAllByText } = simpleRender(
       <Carousel
         title="foo"
@@ -30,5 +32,22 @@ describe('Carousel', () => {
     jest.advanceTimersToNextTimer();
 
     await expect(findAllByText('Bar')).resolves.toBeDefined();
+  });
+
+  it('shows the element clicked on', async () => {
+    const { getByText, findAllByText } = simpleRender(
+      <Carousel
+        title="foo"
+        elements={[
+          { title: 'Foo', text: 'Bar', icon: '' },
+          { title: 'Baz', text: 'Qux', icon: '' }
+        ]}
+      />
+    );
+
+    const button = getByText('Baz');
+    fireEvent.click(button);
+
+    await expect(findAllByText('Qux')).resolves.toBeDefined();
   });
 });
